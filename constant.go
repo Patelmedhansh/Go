96d82686455bf6666a7265c6959bf0287700c5e6
MeDzZ <Medhanshpatel18@gmail.com>
2023-10-01 15:52:34 +0530

    Use of constant in GO
    GO allows the use and computation of constant.
    
    note :- short assignment method ":=" cant be used when using const keyword

---
+++ constant.go
@@ -0,0 +1,18 @@
+package main
+
+import"fmt"
+func  main(){ 
+
+	/*const username = "Medhansh "
+	const age = 19 
+
+	fmt.Println(username) 
+	fmt.Println("Age =",age) */
+
+
+	const numMinutesInhour = 60
+	const numSecondsInMinute = 60
+	const numSecondsInHour = numMinutesInhour * numSecondsInMinute 
+
+	fmt.Println("Number of seconds in an hour:",numSecondsInHour) 
+}
