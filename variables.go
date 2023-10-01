64cfacd96e5741acf00876ccaa36c2283a07b701
MeDzZ <Medhanshpatel18@gmail.com>
2023-10-01 15:37:31 +0530

    Variables in GO

---
+++ variables.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+	var smsSendingLimit int = 2
+	var costPerSms float64 =1.2
+	var hasPermission bool = true
+	var username string = "medhansh"
+
+	fmt.Println("The sms sending limit is ", smsSendingLimit) 
+	fmt.Println("The cost per message is ", costPerSms) 
+	fmt.Println("Permission",hasPermission)
+	fmt.Println("User name ",username) 
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+
+
+}
+
