0adba1e38dc9c6dab47ee2ab0454e237e262aad8
MeDzZ <Medhanshpatel18@gmail.com>
2023-10-01 16:49:43 +0530

    Formating strings in GO

---
+++ formatingStrings.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "Medhansh patel "
+	const openRate = 30.512
+
+  msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent", name , openRate, ) 
+
+	// don't edit below this line
+
+	fmt.Println(msg)
+}
