eb3849f1132c2674b066ab6500065ffd538fbfe6
MeDzZ <Medhanshpatel18@gmail.com>
2023-10-01 15:38:21 +0530

    Short Assignment method
    short assignment method for declaring variables in GO

---
+++ shortAssignment.go
@@ -0,0 +1,12 @@
+package main 
+	import"fmt"
+	func main(){
+		username,age, goodLoking := "Medhansh " ,19 , true
+	 
+
+	fmt.Println("User name is ",username) 
+	fmt.Println("Age - ",age ," year old" ) 
+	fmt.Println("Is Medhansh good Loking ?\n",goodLoking)  
+	}
+	
+	// := is short assignment statement , it infers the type of variable on the basis of its value 
