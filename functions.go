ab3b044bd9a635be329d2ff0725163a1133c4651
MeDzZ <Medhanshpatel18@gmail.com>
2023-10-05 23:56:37 +0530

    Update functions.go

--- functions.go
+++ functions.go
@@ -1,21 +1,19 @@
-package main 
-import "fmt"
+package main
 
-func concat (s1 string , s2 string ) string{
-	return s1 + s2 
+//func concat (s1 string , s2 string ) string{
+//return s1 + s2
 
-}
+//}
 
-func main (){
+func main() {
 
 	/* func (x int , y int )int {
 		return x-y
 	} */
 
-	// the above is function signature, it basically states what funciton does and describes the input and output 
+	// the above is function signature, it basically states what funciton does and describes the input and output
 
-fmt.Println(concat("HEY", "CHEEMKI"))
-fmt.Println(concat("cheemki is ", "chibavli ")) 
-
+	//fmt.Println(concat("HEY", "Medhansh"))
+	//fmt.Println(concat("Medhansh  is ", "Good Loking ! "))
 
 }
